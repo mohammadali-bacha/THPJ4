ae3a00bfa2da1f261a3e36d313a13da4c6aba353
Hugo <hros38@gmail.com>
2018-04-05 17:10:46 +0200

    Avec recherche

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,10 @@ function ShowDiv() {
     document.getElementById("myDIV").style.display = "";
     document.getElementById("BB").style.display='none';
 }
+
+window.onload = function() {
+  document.getElementById('btn').onclick = function()
+  {
+    window.location.href = "https://www.google.fr/#q="+document.getElementById("url").value
+  };
+}
